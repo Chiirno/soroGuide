30dcb20c4d86b928ec88e16e4317ccff9c151321
Chiirno <geniuschiruno@gmail.com>
2020-10-23 03:52:16 -0300

    Updates

---
+++ precache-manifest.805a754facc690c1da05f29d0fc15189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a308d75717ee475190c2d8dd0623c3",
+    "url": "/soroGuide/index.html"
+  },
+  {
+    "revision": "01bb82f17af77c3205d9",
+    "url": "/soroGuide/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "01bb82f17af77c3205d9",
+    "url": "/soroGuide/static/js/2.47deec96.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/soroGuide/static/js/2.47deec96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470cf6edeee9dec26029",
+    "url": "/soroGuide/static/js/main.796a9fea.chunk.js"
+  },
+  {
+    "revision": "3f11937d53e1deee5a24",
+    "url": "/soroGuide/static/js/runtime-main.26062620.js"
+  }
+]);
