fd005363ccf2fd9886972f5bb7fc2caa5ada0846
Chiirno <geniuschiruno@gmail.com>
2020-10-23 04:09:21 -0300

    Updates

--- precache-manifest.bf857baa7db0edcf9b73eeb882bf5bb6.js
+++ precache-manifest.bf857baa7db0edcf9b73eeb882bf5bb6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5e396633640b4a6bb613f970b604ae8",
+    "revision": "c3d455cc35311682ff4644d8e11c0103",
     "url": "/soroGuide/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/soroGuide/static/js/2.47deec96.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c6145d301870b9289fb8",
-    "url": "/soroGuide/static/js/main.50f39c2c.chunk.js"
+    "revision": "5d44663fe83e4b4b54ea",
+    "url": "/soroGuide/static/js/main.83f65a37.chunk.js"
   },
   {
     "revision": "3f11937d53e1deee5a24",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/soroGuide/precache-manifest.a8fe49e3bac5c2b527561c7985e19b03.js"
+  "/soroGuide/precache-manifest.bf857baa7db0edcf9b73eeb882bf5bb6.js"
 );
 
 self.addEventListener('message', (event) => {
