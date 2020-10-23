e41af32428f06a568bf06d9e7ed73242263c9542
Chiirno <geniuschiruno@gmail.com>
2020-10-23 04:03:23 -0300

    pary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import Thugs from './Pages/Thugs';
 import Communions from './Pages/Communions';
