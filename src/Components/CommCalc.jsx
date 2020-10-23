6299b7638303f6d4bbb445c8a31bbd8bd62c2967
Chiirno <geniuschiruno@gmail.com>
2020-10-23 05:57:50 -0300

    more commcalc work

--- src/Components/CommCalc.jsx
+++ src/Components/CommCalc.jsx
@@ -12,22 +12,29 @@ const ComCalc = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-sm p-3 m-1 border border-primary rounded"> <h3>Caster</h3>
-          Paths: <input className="m-1" type="number" value={masterPaths}
-          onChange={e => setMasterPaths(e.target.value)}/><br/>
-          Spell Lvl: <input className="m-1" type="number" value={spellLevel}
-          onChange={e => setSpellLevel(e.target.value)}/><br/>
-          Fatigue: <input className="m-1" type="number" value={spellFatigue}
-          onChange={e => setSpellFatigue(e.target.value)}/><br/>
-          Encumbrace: <input className="m-1" type="number" value={masterEncumbrance}
-          onChange={e => setMasterEncumbrance(e.target.value)}/>
+
+        <div className="col-sm p-3 m-1 border border-primary rounded">
+          <h3 className="text-center pb-2">Caster</h3>
+          <table style={{width: "110%"}}>
+            <tr><td>Paths:</td><td><input className="m-1" type="number" value={masterPaths}
+            onChange={e => setMasterPaths(e.target.value)}/></td></tr>
+            <tr><td>Spell Lvl:</td><td><input className="m-1" type="number" value={spellLevel}
+            onChange={e => setSpellLevel(e.target.value)}/></td></tr>
+            <tr><td>Fatigue:</td><td><input className="m-1" type="number" value={spellFatigue}
+            onChange={e => setSpellFatigue(e.target.value)}/></td></tr>
+            <tr><td>Encumbrace:</td><td><input className="m-1" type="number" value={masterEncumbrance}
+            onChange={e => setMasterEncumbrance(e.target.value)}/></td></tr>
+          </table>
         </div>
 
-        <div className="col-sm p-3 m-1 border border-danger rounded"> <h3>Slave</h3>
-          Paths: <input className="m-1" type="number" value={slavePaths}
-          onChange={e => setSlavePaths(e.target.value)}/><br/>
-          Spell Lvl: <input className="m-1" type="number" value={slaveNumber}
-          onChange={e => setSlaveNumber(e.target.value)}/><br/>
+        <div className="col-sm p-3 m-1 border border-danger rounded">
+        <h3 className="text-center pb-2">Slave</h3>
+        <table>
+          <tr><td>Paths:</td><td><input className="m-1" type="number" value={slavePaths}
+          onChange={e => setSlavePaths(e.target.value)}/></td></tr>
+          <tr><td>Spell Lvl:</td><td><input className="m-1" type="number" value={slaveNumber}
+          onChange={e => setSlaveNumber(e.target.value)}/></td></tr>
+          </table>
         </div>
       </div>
       <p>Value is {masterPaths}</p>
